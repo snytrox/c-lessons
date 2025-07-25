14daf2e937e907d3edf1962bba6d4cef21b9c3f4
snytrox <pcmelekkq@gmail.com>
2025-07-25 21:01:31 +0300

    Update main.c

--- main.c
+++ main.c
@@ -8,11 +8,11 @@ int main()
  setlocale(LC_ALL,"Turkish");
 
 
-//kenar uzunluklarý verilen üçgenlerin hangi tür üçgen olduðunu hesaplayan program
+// a program that calculates the type of triangle given the side lengths of the triangles.
 
    int a=0,b=0,c=0;
    char buyuk;
-   printf("üçgenin kenar uzunluklarýný giriniz:\n");
+   printf("Enter the side lengths of the triangle:\n");
    scanf("%d%d%d",&a,&b,&c);
    if(a+b>c && a+c>b && c+b>a){
         if(a>=b){
@@ -37,7 +37,7 @@ else{
     buyuk='C';
 }
                 }}
-        else{printf("bu üçgen olamaz");}
+        else{printf("this is not a triangle");}
         if(c>=a){
                 if(c-a<b){
                     if(a>=b && a>=c){
@@ -60,7 +60,7 @@ else{
     buyuk='C';
 }
                 }}
-        else{printf("bu üçgen olamaz");}
+        else{printf("this is not a triangle");}
         if(a>=c){
                 if(a-c<b){
                     if(a>=b && a>=c){
@@ -83,39 +83,39 @@ else{
     buyuk='C';
 }
                 }}
-        else{printf("bu üçgen olamaz");}
+        else{printf("this is not a triangle");}
    }
 
 
 
    else{
-    printf("bu üçgen olamaz");
+    printf("this is not a triangle");
    }
 
 if (buyuk=='A'){
     if(a*a == b*b + c*c){
-        printf("dik üçgen");
+        printf("right triangle");
     }else if(a*a<b*b+c*c){
-        printf("küçük açýlý üçgen");
+        printf("small angle triangle");
     }else{
-         printf("geniþ açýlý üçgen");
+         printf("obtuse angle triangle");
     }
 }else if(buyuk=='B'){
     if(b*b==a*a+c*c){
-        printf("dik üçgen");
+        printf("right triangle");
     }else if(b*b<a*a+c*c){
-        printf("küçük açýlý üçgen");
+        printf("small angle triangle");
     }else{
-         printf("geniþ açýlý üçgen");
+         printf("obtuse angle triangle");
     }
 }
 else
  if(c*c==(a*a)+(b*b)){
-        printf("dik üçgen");
+        printf("right triangle");
     }else if(c*c<(a*a)+(b*b)){
-        printf("küçük açýlý üçgen");
+        printf("small angle triangle");
     }else{
-         printf("geniþ açýlý üçgen");
+         printf("obtuse angle triangle");
     }
 
 
